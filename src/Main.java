ecd7c407eef93f88c5b5a2ec5136c580b53ab343
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-10-18 12:03:07 +0100

    Feature: se añadió la funcionaliad del programa
    Se inician las ventanas que el usuario diga y una vez abietas, 500ms después se cierran

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder("notepad.exe");
+        Process p = null;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Número de ventanas: ");
+
+        int numeroVentanas = sc.nextInt();
+
+        ArrayList<Process> lista = new ArrayList<>();
+        sc.nextLine();
+        for (int i = 0; i < numeroVentanas; i++) {
+            p = pb.start();
+            lista.add(p);
+            System.out.println("El porceso " + (i +1) + " se inició a : " + LocalTime.now().toString().substring(0,12));
+        }
+        lista.get(0).waitFor(500, TimeUnit.MILLISECONDS);
+        for (int i = 0; i < lista.size(); i++) {
+            lista.get(i).destroyForcibly();
+            System.out.println("El porceso " + (i +1) + " se terminó a : " + LocalTime.now().toString().substring(0,12));
+        }
     }
 }
